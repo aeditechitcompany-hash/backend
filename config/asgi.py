63daa439631c802f20b6a7129ff75f4693e7e5bf
ayushbhujel <bhujelaayush47@gmail.com>
2026-08-31 07:42:29 +0545

    Comment added

--- config/asgi.py
+++ config/asgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
-
+#myedits
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
