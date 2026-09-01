061217b308972a671b209370d84638aed1cda9d3
ayushbhujel <bhujelaayush47@gmail.com>
2026-09-01 16:44:40 +0545

    changing Cloudinary code

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,14 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import (SpectacularAPIView,SpectacularSwaggerView,SpectacularRedocView,)
 
+def home(request):
+    return JsonResponse({
+        "status": "ok",
+        "message": "Backend is running"
+    })
+
 urlpatterns = [
+     path("", home),
     path("admin/", admin.site.urls),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui",
