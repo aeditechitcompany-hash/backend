3f843b59ab5bdce5253452973dc6e85725fdd2c5
ayushbhujel <bhujelaayush47@gmail.com>
2026-09-01 16:20:16 +0545

    Fix settings.py

--- config/settings.py
+++ config/settings.py
@@ -265,3 +265,19 @@ SPECTACULAR_SETTINGS = {
     },
 }
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
